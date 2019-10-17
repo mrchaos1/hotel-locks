73a328f61932afa77d3ee43ba3303961ed89f73a
mrchaos1 <mrchaos001@yandex.ru>
2019-10-17 12:14:03 +0700

    add regex

--- src/Service/XeederKeyService.php
+++ src/Service/XeederKeyService.php
@@ -98,6 +98,8 @@ class XeederKeyService extends KeyService implements KeyServiceInterface
             'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ü' => 'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ҡ' => 'k',
         ]);
 
+        $guestName = preg_replace('/[^А-Яа-яA-Za-z0-9,.;:"!@№$%^&*#()_+-=* ]+/', '?', $guestName);
+
         $command =
             $ddssAddress  . self::FIELD_GUEST_CHECK_IN
             . chr(124) . self::FIELD_ROOM_NUMBERS . implode(',', $room->getDoorCodes())
